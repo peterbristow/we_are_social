43cecc0d0b0b5ac9892f019a1f6e347dd3d4c2c9
Peter <peterjb73@gmail.com>
2016-05-27 16:13:39 +0100

    Connect to database.

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,12 @@
+import os
+
 from base import *
+import dj_database_url
 
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(os.environ.get('CLEARDB_DATABASE_URL'))
 }
 
 # Stripe environment variables
